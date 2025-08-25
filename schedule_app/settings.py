cf69955a4a08e4cf6a9e69f25ed8da8fea14c448
sato yukiya <yukiya.sato@rakus-partners.co.jp>
2025-08-25 18:01:25 +0900

    db用 コンテナの作成

--- schedule_app/settings.py
+++ schedule_app/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'schedule_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "schedule_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",   # docker-compose のサービス名
+        "PORT": "5432",
     }
 }
 
