33ebc3854c67ed16cff4d00d1e5bb263f70372e4
sato yukiya <yukiya.sato@rakus-partners.co.jp>
2025-08-27 01:19:13 +0900

    フロントページの初期作成

--- events/views.py
+++ events/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+def event_list(request):
+    events = Event.objects.all().order_by('start_time')
+    return render(request, 'events/event_list.html', {'events': events})
